06755ed940f8d5eecbcf3fdf82575275b6d17237
kennedy <dkennyvs@gmail.com>
2022-06-11 15:02:18 -0300

    Fin API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifiryIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({error: 'Customer not found!'});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 /**
 * cpf - string
 * name - string
@@ -34,15 +49,29 @@ app.post('/account', (request, response) => {
     return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-    const { cpf } = request.headers;
+// app.use(verfiryIfExistsAccountCPF);
 
-    const customer = customers.find(customer => customer.cpf === cpf);
-    if (!customer) {
-        return response.status(400).json({error: 'Customer not found!'});
-    }
+app.get('/statement', verifiryIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
+});
+
+app.post('/deposit', verifiryIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+    
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
+    }
+
+    customer.statement.push(statementOperation);
+    
+    return response.status(201).send();
 });
 
 app.listen(6666);
