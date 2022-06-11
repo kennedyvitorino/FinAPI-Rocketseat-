63ab25db683b011235c7edccc2e85859c66e1bc0
kennedy <dkennyvs@gmail.com>
2022-06-11 16:53:58 -0300

    Fin API

--- src/index.js
+++ src/index.js
@@ -107,4 +107,16 @@ app.post('/withdraw', verifiryIfExistsAccountCPF, (request, response) => {
     return response.status(201).send();
 });
 
+app.get('/statement/date', verifiryIfExistsAccountCPF, (request, response) => {
+    const { customer} = request;    
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + ' 00:00:00');
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+    return response.json(statement);
+
+});
+
 app.listen(6666);
